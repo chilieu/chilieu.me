673c796b3339d8ebb82255b1b7980203c7a47655
Chi T Lieu <lieu.thanh.chi@gmail.com>
2015-09-29 02:31:19 -0400

    v0.0.1

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <!DOCTYPE HTML>
-<!--
-	Fractal by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<head>
 		<title>Chi T Lieu | PHP Developer</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
@@ -26,7 +33,9 @@
 						<li><a href="#one" class="button icon fa-chevron-down scrolly">Learn More What I-Do</a></li>
 					</ul>
 				</div>
-				<div class="image phone"><div class="inner"><img src="images/screen.jpg" alt="" /></div></div>
+				<div class="image img-responsive"><div class="inner">
+					<img src="images/chilieu.jpg" alt="ChiLT | PHP Developer" class="img-responsive" />
+				</div></div>
 			</header>
 
 		<!-- One -->
@@ -40,16 +49,105 @@
                         <br> <a href="http://www.abmx.com" rel="nofollow">ABMX Servers</a>, <a href="http://www.bakerhughes.com/" rel="nofollow">Baker Hughes Inc.</a> in America.
                                                 <br>I mostly enjoy working on small to medium-sized projects.</span>
 				</header>
+					<div class="inner alt">
+						<h2>My Skills and Weapons</h2>
+						<div class="table-wrapper">
+							<table class="alt">
+								<thead>
+									<tr>
+										<th>Skill</th>
+										<th>Description</th>
+										<th>Year(s)</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>Back-End</td>
+										<td>PHP5 - MySQL, CodeIgniter Framework, Phalcon Framework</td>
+										<td>3</td>
+									</tr>
+									<tr>
+										<td>Front-End</td>
+										<td>Angularjs, WordPress Magic, CSS/HTML + Ajax/JQuery, Bootstrap Framework</td>
+										<td>3</td>
+									</tr>
+									<tr>
+										<td>Mobile Apps</td>
+										<td>Ionic, Android Apps, Android Games, Mobile-Responsive Websites</td>
+										<td>1</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+
+
 				<ul class="actions">
-					<li><a href="#three" class="button icon fa-chevron-down scrolly">Learn More</a></li>
+					<li><a href="#two" class="button icon fa-chevron-down scrolly">Learn More</a></li>
 				</ul>
 			</section>
 
+		<!-- Two -->
+			<section id="two" class="wrapper">
+				<div class="inner alt">
+					<section class="spotlight">
+						<div class="image"><img src="images/projects/gaffney-cline-associates.jpg" alt="" /></div>
+						<div class="content">
+							<h3>Gaffney Cline</h3>
+							<p>BakerHughes Micro-site
+								<br>WordPress Web Responsive
+								<br>Year: 2014</p>
+						</div>
+					</section>
+					<section class="spotlight">
+						<div class="image"><img src="images/projects/kessen.jpg" alt="Kessen Game" /></div>
+						<div class="content">
+							<h3>Kessen Web Game</h3>
+							<p>Images Processing Game
+								<br>PHP - MySQL, Mobile Web
+								<br>Year: 2013</p>
+						</div>
+					</section>
+					<section class="spotlight">
+						<div class="image"><img src="images/projects/stove.jpg" alt="" /></div>
+						<div class="content">
+							<h3>Stove Web Branding.</h3>
+							<p>WordPress CMS
+								<br>WordPress - PHP - MySQL
+								<br>Year: 2012</p>
+						</div>
+					</section>
+
+				</div>
+			</section>
+
 		<!-- Three -->
-			<section id="three" class="wrapper style2 special">
+
+		<section id="three" class="wrapper">
+			<div class="inner">
+			<div class="box alt">
+				<div class="row uniform">
+					<div class="4u"><span class="image fit"><img src="images/projects/abmx-server.jpg" alt="" /></span></div>
+					<div class="4u"><span class="image fit"><img src="images/projects/backend-project.jpg" alt="" /></span></div>
+					<div class="4u"><span class="image fit"><img src="images/projects/bibile-trivia.jpg" alt="" /></span></div>
+					<div class="4u"><span class="image fit"><img src="images/projects/bible-phalcon.jpg" alt="" /></span></div>
+					<div class="4u"><span class="image fit"><img src="images/projects/magnitude.jpg" alt="" /></span></div>
+					<div class="4u"><span class="image fit"><img src="images/projects/sitell.jpg" alt="" /></span></div>
+					<div class="4u"><span class="image fit"><img src="images/projects/tiasang.jpg" alt="" /></span></div>
+					<div class="4u"><span class="image fit"><img src="images/projects/master-golf-2009.jpg" alt="" /></span></div>
+					<div class="4u"><span class="image fit"><img src="images/projects/bibile-trivia.jpg" alt="" /></span></div>
+				</div>
+			</div>
+			</div>
+		</section>
+
+		<!-- Four -->
+			<section id="four" class="wrapper style2 special">
 				<header class="major">
 					<h2>My Resume</h2>
-					<p>Want to hire me for a project, got questions about my work or just want to say hi?</p>
+					<p>Want to hire me for a project, got questions about my work or just want to say hi?
+						<br>Just send me an message via Social Network below!
+					</p>
 				</header>
 				<ul class="actions">
 					<li><a href="./CHILT-PHP-Developer.pdf" class="button special icon fa-download">Download</a></li>
@@ -68,7 +166,19 @@
 					<li><a href="https://plus.google.com/+lieuthanhchi/about/p/pub" class="icon fa-google-plus"><span class="label">Instagram</span></a></li>
 					<li><a href="https://www.linkedin.com/pub/chi-lieu/33/32/2ab" class="icon fa-linkedin"><span class="label">Instagram</span></a></li>
 				</ul>
-				<p class="copyright">&copy; Credits: <a href="#">HTML5 UP</a></p>
+				<p class="copyright">Chi T Lieu</p>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- chilieu_me_demo -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-0760902944328301"
+     data-ad-slot="7683789620"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 			</footer>
 
 		<!-- Scripts -->
@@ -81,3 +191,14 @@
 
 	</body>
 </html>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59474380-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
